0a8cb16bafa1f77c552cda3f02da2d468cb7a4cb
gyurili <inglifestora@naver.com>
2023-08-10 21:41:46 +0900

    임시 커밋

--- savior/urls.py
+++ savior/urls.py
@@ -5,10 +5,17 @@ app_name='savior'
 
 urlpatterns = [
     path('', views.main , name='main'),
+    #일본
     path('japan/', views.japan , name='japan'),
     path('japan_exchange/', views.japan_exchange , name='japan_exchange'),
+    path('japan_pricelist/', views.japan_pricelist , name='japan_pricelist'),
+    path('japan_clothes/', views.japan_clothes , name='japan_clothes'),
+    path('japan_foods/', views.japan_foods , name='japan_foods'),
+    path('japan_others/', views.japan_others , name='japan_others'),
+    #미국
     path('USA/', views.USA , name='USA'),
     path('USA_exchange/', views.USA_exchange , name='USA_exchange'),
+    #베트남
     path('vietnam/', views.vietnam , name='vietnam'),
     path('vietnam_exchange/', views.vietnam_exchange , name='vietnam_exchange'),
     
--- savior/views.py
+++ savior/views.py
@@ -25,6 +25,22 @@ def japan(request):
     }
     return render(request, "japan.html", context)
 
+#일본 시세 페이지
+def japan_pricelist(request):
+    return render(request, "japan_pricelist.html")
+
+#일본 의류 시세 페이지
+def japan_clothes(request):
+    return render(request, "japan_clothes.html")
+
+#일본 음식 시세 페이지
+def japan_foods(request):
+    return render(request, "japan_foods.html")
+
+#일본 잡화 시세 페이지
+def japan_others(request):
+    return render(request, "japan_others.html")
+
 #일본 날씨
 def japan_weather():
     city = "Tokyo"
@@ -97,159 +113,74 @@ def USA_weather():
 
     return clouds_info, icon_info, temperature
 
-
-
-
 #미국 환율계산기
 
 def get_exchange_rate2():
-
     headers = {
-
         'User-Agent': 'Mozilla/5.0',
-
         'Content-Type': 'text/html; charset=utf-8'
-
     }
-
-
-
-
     scraper = cloudscraper.create_scraper()
-
     url = "https://kr.investing.com/currencies/usd-krw"
-
     html = scraper.get(url, headers=headers).content
-
     soup = BeautifulSoup(html, 'html.parser')
-
     containers = soup.find('span', {'data-test': 'instrument-price-last'})
-
     exchange_rate = containers.text if containers else None
-
-
-
 
     return exchange_rate
 
-
-
-
 def USA_exchange(request):
-
     if request.method == 'POST':
-
         USD = request.POST.get('USD', 0)
-
         USD = int(USD)
-
         exchange_rate = get_exchange_rate2()
-
-
-
-
         if exchange_rate:
-
             exchange_rate = float(exchange_rate.replace(',', ''))
-
             KRW = USD * exchange_rate
-
             return render(request, 'USA_exchange.html', {'USD': USD, 'KRW': KRW, 'exchange_rate': exchange_rate})
-
         else:
-
             return render(request, 'USA_exchange.html', {'USD': USD, 'exchange_rate': 'Error'})
-
     else:
-
         return render(request, 'USA_exchange.html')
-
-
-
 
 #베트남 상세페이지
 
 def vietnam(request):
-
     exchange_rate = 5.47
-
     clouds_info, icon_info, temperature = vietnam_weather()
-
     context = {
-
         'clouds_info': clouds_info,
-
         'icon_info': icon_info,
-
         'temperature': temperature,
-
         'exchange_rate': exchange_rate,
-
     }
-
     return render(request, "vietnam.html", context)
 
-
-
-
 #베트남 날씨
-
 def vietnam_weather():
-
     city = "Hanoi"
-
     apikey = "1a34ea4698296cf6cb4bb168b8356219"
-
     lang = "kr"
-
     api = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apikey}&lang={lang}&units=metric"
-
     result = requests.get(api)
-
     data = json.loads(result.text)
-
-
-
-
     clouds_info = data.get('weather', [{'main': 'N/A'}])[0]['main']  # 하늘 상태
-
     icon_info = data.get('weather', [{'icon': 'N/A'}])[0]['icon']  # 아이콘
-
     temperature = data.get('main', {'temp': 'N/A'})['temp']  # 기온
-
-
-
 
     return clouds_info, icon_info, temperature
 
-
-
-
 #베트남 환율계산기
-
 def vietnam_exchange(request):
-
     if request.method == 'POST':
-
         VND = request.POST['VND']
-
         VND = int(VND)
-
         exchange_rate = 5.47 * 0.01
-
         KRW = VND * exchange_rate
-
         return render(request,'vietnam_exchange.html',{'VND':VND,'KRW':KRW})
-
     else:
-
         return render(request, 'vietnam_exchange.html')
 
-
-
-
 #임시 마이페이지
-
 def mypage(request):
-
     return render(request, "mypage.html")
