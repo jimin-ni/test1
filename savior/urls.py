a970a6f737bd1a93d22d6fd534dbd9513455ec3f
gyurili <inglifestora@naver.com>
2023-08-13 05:10:08 +0900

    마이페이지, 시세 입력창 기본틀

--- savior/urls.py
+++ savior/urls.py
@@ -15,22 +15,33 @@ urlpatterns = [
     path('japan_exchange/', views.japan_exchange , name='japan_exchange'),
     path('japan_pricelist/', views.japan_pricelist , name='japan_pricelist'),
     path('japan_clothes/', views.japan_clothes , name='japan_clothes'),
+    path('japan_clothes_detail/<int:id>', views.japan_clothes_detail , name='japan_clothes_detail'),
     path('japan_foods/', views.japan_foods , name='japan_foods'),
+    path('japan_foods_detail/<int:id>', views.japan_foods_detail , name='japan_foods_detail'),
     path('japan_others/', views.japan_others , name='japan_others'),
+    path('japan_others_detail/<int:id>', views.japan_others_detail , name='japan_others_detail'),
+    
     #미국
-    path('USA/', views.USA , name='USA'),
-    path('USA_exchange/', views.USA_exchange , name='USA_exchange'),
-    path('USA_pricelist/', views.USA_pricelist , name='USA_pricelist'),
+    path('USA/', views.usa , name='USA'),
+    path('USA_exchange/', views.usa_exchange , name='USA_exchange'),
+    path('USA_pricelist/', views.usa_pricelist , name='USA_pricelist'),
     path('USA_clothes/', views.usa_clothes , name='USA_clothes'),
+    path('USA_clothes_detail/<int:id>', views.usa_clothes_detail , name='USA_clothes_detail'),
     path('USA_foods/', views.usa_foods , name='USA_foods'),
+    path('USA_foods_detail/<int:id>', views.usa_foods_detail , name='USA_foods_detail'),
     path('USA_others/', views.usa_others , name='USA_others'),
+    path('USA_others_detail/<int:id>', views.usa_others_detail , name='USA_others_detail'),
+
     #베트남
     path('vietnam/', views.vietnam , name='vietnam'),
     path('vietnam_exchange/', views.vietnam_exchange , name='vietnam_exchange'),
     path('vietnam_pricelist/', views.vietnam_pricelist , name='vietnam_pricelist'),
     path('vietnam_clothes/', views.vietnam_clothes , name='vietnam_clothes'),
+    path('vietnam_clothes_detail/<int:id>', views.vietnam_clothes_detail , name='vietnam_clothes_detail'),
     path('vietnam_foods/', views.vietnam_foods , name='vietnam_foods'),
+    path('vietnam_foods_detail/<int:id>', views.vietnam_foods_detail , name='vietnam_foods_detail'),
     path('vietnam_others/', views.vietnam_others , name='vietnam_others'),
+    path('vietnam_others_detail/<int:id>', views.vietnam_others_detail , name='vietnam_others_detail'),
     
     #* 로그인 및 회원가입
     path('accounts/', include('accounts.urls', namespace='accounts' )),
