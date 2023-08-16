6e4071bc27998e37a1c03c7b43fe02143d73555e
ose0221 <ose0221@naver.com>
2023-08-17 02:54:10 +0900

    login 연결 수정

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import TextInput, EmailInput, PasswordInput
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
@@ -12,6 +13,23 @@ class UserCreateForm(UserBaseForm):
     password2 = forms.CharField(widget=forms.PasswordInput())
     class Meta(UserBaseForm.Meta):
         fields = ['username', 'email', 'password']
+        widgets = {
+            'username': TextInput(attrs={
+                'class': "form-control",
+                'style': 'width: 320px;',
+                'placeholder': '아이디 입력'
+                }),
+            'email': EmailInput(attrs={
+                'class': "form-control",
+                'style': 'width: 320px;',
+                'placeholder': 'Email'
+                }),
+            'password': PasswordInput(attrs={
+                'class': "form-control",
+                'style': 'width: 320px;',
+                'placeholder': '비밀번호 입력'
+            }),
+        }
 
 #* 기존 제공 form(이메일 포함)
 class SignUpForm(UserCreationForm):
